1718b65c3727abdc734c8bbb95c423788273a91e
Drew LeSueur <drewalex@gmail.com>
2014-05-18 21:32:11 +0000

    ws and 7x7

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 // 64x64 operating system
 //redis based.
 
+var thew = 7
 var http = require("http")
 
 var redis = require("redis")
@@ -18,19 +19,75 @@ var fs = require("fs")
 var _ = require("underscore")
 
 var listeners = {}
+var listenersWs = {}
+
+setInterval(function () {
+  var json = getMessageJson()
+  //_.each(listeners, function (res) {
+  //  res.write("data: " + json + "\n\n")
+  //})
+
+  _.each(listenersWs, function (ws) {
+    ws.send(json)
+  })
+}, 500)
+
+var allColors = ["red", "green", "blue", "pink", "lime", "cyan", "magenta", "yellow", "orange"]
+//var allColors = ["black", "white"]
+var allLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890"
+//var allColors = [0, 1]
+//var allColors = ["#ff0000", "#00ffff", "#00ff00", "orange"]
+
+var getMessageJson = function () {
+  var colors = []
+  var letters = []
+  var w = thew
+  var h = w
+  _.times(w * h, function () {
+    colors.push(allColors[_.random(0, allColors.length - 1)]) 
+    letters.push(allLetters[_.random(0, allLetters.length - 1)]) 
+  })
+  //letters = "hello world".split("")
+  var ret = {
+    x: 0,
+    y: 0,
+    w: w,
+    h: h,
+    colors: colors,
+    letters: letters,
+  }
+  return JSON.stringify(ret)
+}
+
+var WebSocketServer = require('ws').Server
+var wss = new WebSocketServer({port: 1618});
+
+wss.on('connection', function(ws) {
+  var id = _.uniqueId("listener")
+  ws.__my_id = id
+  listenersWs[id] = ws
+  ws.on('close', function () {
+    delete listenersWs[id] 
+  }) 
+  ws.on('message', function(message) {
+  });
+  //ws.send('something');
+});
+
 http.createServer(function (req, res) {
   var url_parts = url.parse(req.url, true);
-  console.log(url_parts)
   var query = url_parts.query;
   var pathname = url_parts.pathname
+  console.log(pathname)
   if (pathname == "/") {
     res.writeHead(200, {'Content-Type': 'text/html'});
     fs.readFile("./index.html", function (err, text) {
-      res.end(text.toString()) 
+      res.end(text.toString().replace(/"thew"/, thew)) 
     }) 
-  } else if (pathname == "/touch") {
+  } else if (pathname == "/in") {
     res.end();
-  } else if (pathname == "/events") {
+  } else if (pathname == "/out") {
+    console.log("out!")
     var id = _.uniqueId("listener")
     res.__my_id = id
     listeners[id] = res
@@ -38,8 +95,6 @@ http.createServer(function (req, res) {
       delete listeners[id] 
     }) 
     res.writeHead(200, {'Content-Type': 'text/event-stream'});
-    res.end()
-
   } else {
     res.end()
   }
